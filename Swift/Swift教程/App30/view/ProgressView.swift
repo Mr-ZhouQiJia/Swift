ba2904b82b4322a61d82573b17ca596b2d6875ae
zzx <>
2018-09-18 15:55:56 +0800

    no message

--- Swift/Swift教程/App30/view/ProgressView.swift
+++ Swift/Swift教程/App30/view/ProgressView.swift
@@ -43,19 +43,22 @@ class ProgressView: UIView {
     /**
      进度
      */
-//    private var progressvalue : CGFloat? = CGFloat()
-    var progressValue : CGFloat?
-//    {
-//        set{
-//            print(newValue )
-//            progressvalue = newValue
-//            self.progressView?.frame = CGRect(x: (self.progressView?.frame.origin.x)!, y: (self.progressView?.frame.origin.y)!, width: (self.bottonView?.frame.size.width)! * newValue, height: (self.progressView?.frame.size.height)!)
-//
-//
-//        }get{
-//            return progressvalue!
-//        }
-//    }
+    private var progressvalue : CGFloat? = CGFloat()
+    var progressValue : CGFloat
+    {
+        set{
+            print(newValue as Any )
+            progressvalue = newValue
+            UIView.animate(withDuration: 5) {
+                self.progressView?.frame = CGRect(x: (self.progressView?.frame.origin.x)!, y: (self.progressView?.frame.origin.y)!, width: (self.bottonView?.frame.size.width)! * newValue, height: (self.progressView?.frame.size.height)!)
+            }
+            
+
+
+        }get{
+            return progressvalue!
+        }
+    }
     
     /**
     进度条速度
@@ -72,10 +75,24 @@ class ProgressView: UIView {
      */
     var  progressView : UIView?
     
-    override init(frame: CGRect) {
-       super.init(frame: frame)
+   convenience init(frame: CGRect , progressBackColor :UIColor ,progressViewColor : UIColor , progressValue : CGFloat ) {
+       self.init(frame: frame)
        self.buildUI()
-        
+       self.progressBackColor = progressBackColor
+       self.progressViewColor = progressViewColor
+       self.progressValue = progressValue
+    }
+    
+    convenience init( progressBackColor :UIColor ,progressViewColor : UIColor , progressValue : CGFloat ) {
+        self.init()
+        self.buildUI()
+        self.progressBackColor = progressBackColor
+        self.progressViewColor = progressViewColor
+        self.progressValue = progressValue
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -95,15 +112,17 @@ class ProgressView: UIView {
         self.bottonView = UIView(frame: self.bounds)
         
         self.addSubview(self.bottonView!)
-        self.bottonView?.backgroundColor = UIColor.lightGray
+        self.bottonView?.backgroundColor = self.progressBackColor
         self.bottonView?.layer.masksToBounds = true
         self.bottonView?.layer.cornerRadius = 3
         self.progressView = UIView()
         
         self.bottonView?.addSubview(self.progressView!)
         self.progressValue = 0.8
-        self.progressView?.frame = CGRect(x: (self.progressView?.frame.origin.x)!, y: (self.progressView?.frame.origin.y)!, width: (self.bottonView?.frame.size.width)! * progressValue!, height: (self.progressView?.frame.size.height)!)
-        self.progressView?.backgroundColor = UIColor.red
+    
+        self.progressView?.frame = CGRect(x: 0, y: 0, width: 0, height: (self.bottonView?.frame.size.height)!)
+        
+        self.progressView?.backgroundColor = self.progressViewColor
         self.progressView?.layer.masksToBounds = true
         self.progressView?.layer.cornerRadius = 3
     }
